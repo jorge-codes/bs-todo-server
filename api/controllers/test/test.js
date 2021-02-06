d767df10d48a1fe4b5d50f46916b418ec9504d5c
Jorge Palacios <reefaktor@gmail.com>
2021-02-06 13:09:23 -0300

    Implemented test endpoint for Heroku

---
+++ api/controllers/test/test.js
@@ -0,0 +1,30 @@
+module.exports = {
+
+
+  friendlyName: 'Test',
+
+
+  description: 'Test test.',
+
+
+  inputs: {
+  },
+
+
+  exits: {
+  },
+
+
+  fn: async function () {
+
+    const response = {
+      message: 'It works!'
+    };
+
+
+    // All done.
+    return response;
+  }
+
+
+};
