b1a78713d2f55be55a30b8471edcd0e4afb802a0
Jorge Palacios <reefaktor@gmail.com>
2021-02-16 22:06:35 -0300

    Updated CORS settings
    * Updated CORS settings to be able to test React App locally before moving to Netlify

--- config/security.js
+++ config/security.js
@@ -28,11 +28,13 @@ module.exports.security = {
   *                                                                          *
   ***************************************************************************/
 
-  // cors: {
-  //   allRoutes: false,
-  //   allowOrigins: '*',
-  //   allowCredentials: false,
-  // },
+  cors: {
+    allRoutes: true,
+    allowOrigins: '*',
+    allowRequestMethods: 'GET,POST,PUT,DELETE,OPTIONS,HEAD',
+    allowRequestHeaders: 'content-type',
+    allowCredentials: false
+  },
 
 
   /****************************************************************************
